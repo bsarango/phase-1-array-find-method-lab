0fe1fdc3dd92c1a1b4c3d009943f2b29a6f56318
Brian Sarango <brian.sarango@baruchmail.cuny.edu>
2023-05-01 20:05:20 -0400

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // code your solution here
+function findWin(record){
+    return record.result === "W"
+}
+
+function superbowlWin(teamRecord){
+    let win = teamRecord.find(findWin)
+    if(win){
+        return win.year
+    }
+    else{
+        return undefined
+    }
+}
